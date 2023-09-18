f4364a7741f56685d51047464f875316a766b99b
nehadadel <eng.nehadadel@gmail.com>
2023-09-18 15:36:28 -0700

    edit_printf

--- main.h
+++ main.h
@@ -7,8 +7,6 @@
 #include <stddef.h>
 
 int _printf(const char *format, ...);
-int print_string(char * str);
-int print_char(char ch);
 char itoa_generic (char* u8ptrbuf, int u32int_value);
 void putprin(char *t ,long double dig, char fly, char *ss);
 
