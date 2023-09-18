a0aa5f07c8a42d6c7ffcccefe356006c05240991
nehadadel <eng.nehadadel@gmail.com>
2023-09-18 15:13:06 -0700

    main.h

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+int _printf(const char *format, ...);
+int print_string(char * str);
+int print_char(char ch);
+char itoa_generic (char* u8ptrbuf, int u32int_value);
+void putprin(char *t ,long double dig, char fly, char *ss);
+
+#endif
