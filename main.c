3cf13e2f83b469e38a9593413f5a422549d92115
Alshimaa Mamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2023-09-19 19:36:55 -0700

    updated

--- main.c
+++ main.c
@@ -41,8 +41,7 @@ len = _printf("Let's try to printf a simple sentence.\n");
     _printf("String:[%s%s]\n", "I am a string !", "second string !");
     printf("String:[%s%s]\n", "I am a string !", "second string !");
 
-   _printf("String:[%s%s]\n", "I am a string !", "second string !");
-    printf("String:[%s %s]\n", "I am a string !", "second string !");
+
 
     return (0);
 }
