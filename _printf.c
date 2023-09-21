8e72d552c026cc5bd8344250511a9c505a99b405
nehadadel <eng.nehadadel@gmail.com>
2023-09-21 08:06:32 -0700

    yarb

--- _printf.c
+++ _printf.c
@@ -30,6 +30,8 @@ if (format[n] == '%')
 {
 num_of_ch += write(1, &format[n], 1);
 }
+else
+{ 
 sp_result = get_sp_func(format[n]);
 if (sp_result != NULL)
 num_of_ch += sp_result(args);
@@ -37,6 +39,7 @@ else
 {
 num_of_ch += write(1, &format[n - 1], 1);
 num_of_ch += write(1, &format[n], 1);
+}
 }
 }
 n++;
