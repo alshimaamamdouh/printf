fcb79025144824b6c63966cbab1b7a39e0015679
nehadadel <eng.nehadadel@gmail.com>
2023-09-22 16:01:11 -0700

    print_pointer

--- print_pointer.c
+++ print_pointer.c
@@ -11,15 +11,19 @@ int print_pointer(va_list args)
 size_t i;
 uintptr_t address = va_arg(args, uintptr_t);
 char * array_address;
+int num_of_ch = 0;
 
 array_address = malloc(sizeof(address));
 if (array_address == NULL)
 return (NULL);
 
 for (i = 0; i < sizeof(address); i++) {
-charArray[i] = (char)((address >> (i * 8)) & 0xFF);
+array_address[i] = (char)((address >> (i * 8)) & 0xFF);
 
-write(1, '0', 1);
-write(1, 'x', 1);
+num_of_ch  += write(1, '0', 1);
+num_of_ch  += write(1, 'x', 1);
 
+
+free(array_address);
+return (num_of_ch);
 }
